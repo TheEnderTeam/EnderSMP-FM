48e2dbaebc4b666d50ae791fae9eeedb408011ed
wrldmap <123031534+wrldmap@users.noreply.github.com>
2024-08-11 12:36:51 -0500

    ?

--- computercraft/client.lua
+++ computercraft/client.lua
@@ -4,7 +4,7 @@ while true do -- Loop
  local wsmsg = ws.receive() -- Read from WS
  local msg = tostring(wsmsg) -- Stringify
  print(msg) -- print song name
- os.sleep(5) -- wait
+ os.sleep(5)
 end
  
 chatBox.sendMessage(msg) -- chat
