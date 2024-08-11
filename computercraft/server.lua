e89666f5be787dc8ccfb7e1faa351609f1938fdf
wrldmap <123031534+wrldmap@users.noreply.github.com>
2024-08-11 12:14:03 -0500

    Update server.lua

--- computercraft/server.lua
+++ computercraft/server.lua
@@ -1,6 +1,7 @@
 ws = http.websocket("ws://localhost:5656") -- ws server
-local wsmsg = io.open(currentsong.txt, "r") -- find current song
+local wsmsg = io.open(currentsong.txt, "r") -- open txt
 local msg = tostring(wsmsg) -- stringify
+io.close(currentsong.txt) -- close txt
 
 ws.send(msg) -- send
 ws.close()
