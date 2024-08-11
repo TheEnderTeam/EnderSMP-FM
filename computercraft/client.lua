68de1683d8ed447f6d55540f87b5b716a7cb6efa
wrldmap <123031534+wrldmap@users.noreply.github.com>
2024-08-11 12:03:01 -0500

    Update client.lua

--- computercraft/client.lua
+++ computercraft/client.lua
@@ -3,6 +3,8 @@ ws = http.websocket("ws://localhost:5656") -- Websocket URL
 while true do -- Loop
  local wsmsg = ws.receive() -- Read from WS
  local msg = tostring(wsmsg) -- Stringify
+ print(msg) -- print song name
+ wait(20) -- wait
 end
  
 chatBox.sendMessage(msg) -- chat
